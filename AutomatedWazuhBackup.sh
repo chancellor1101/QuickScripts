c58289a5dbc33dffbc980e132cb3f0c2aaefbfcd
Clint Chance <clint@thechance.family>
2024-02-15 13:22:24 -0600

    Update AutomatedWazuhBackup.sh

--- AutomatedWazuhBackup.sh
+++ AutomatedWazuhBackup.sh
@@ -3,11 +3,12 @@
 # Set variables for current date and destination folder
 current_date=$(date +%Y-%m)
 destination_folder="/mnt/wasabi/$current_date"
+originating_folder="/var/ossec/logs/archive/"
 
 # Create the destination folder if it doesn't exist
 mkdir -p "$destination_folder"
 
 # Find all files with the specified extensions
-find . -type f \( -name "*.sum" -or -name "*.gz" \) -exec mv -t "$destination_folder" {} \;
+find "$originating_folder" -type f \( -name "*.sum" -or -name "*.gz" \) -exec cp -t "$destination_folder" {} \;
 
 echo "Files moved successfully to $destination_folder"
